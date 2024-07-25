cde0d33707f58587caf07d964a766af52211e5da
SaraMeer453 <i210644@nu.edu.pk>
2024-07-25 14:53:21 +0500

    first commit

---
+++ traffixapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+import Home from './Screens/Home';
+
+function App() {
+  return (
+
+    <div> 
+   <div> <Home/> </div>
+
+
+
+
+   </div>
+  )
+}
+
+export default App;
